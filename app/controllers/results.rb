c6c70dfe5969d6fd317ab670f925ba86f5c53098
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-07 15:34:22 -0500

    finish mvp results page

--- app/controllers/results.rb
+++ app/controllers/results.rb
@@ -1,5 +1,7 @@
 get '/survey/:id/results' do
   @survey = Survey.find(params[:id])
+  @participants = Participation.where(survey_id: params[:id]).count
+  @completed_count = Participation.where(survey_id: params[:id], completion: true).count
 
   erb :results
 end
