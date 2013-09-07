f753bab5c3a7cbddbe7842a121c625f3e7aa7dcf
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-07 14:26:46 -0500

    Fixed Assisiation Betwen authored and taken surveys, fixed part of the tewo lists on profile

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,7 +2,7 @@ class Survey < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   
   has_many :participations
-  has_many :users, through: :participations
+  has_many :survey_takers, through: :participations, source: :user
   has_many :responses, through: :participations
 
   has_many :questions
