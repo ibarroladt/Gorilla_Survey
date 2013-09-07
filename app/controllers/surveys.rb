37684e0fbc61c2d8847e4a4da4db7d3adf7a0613
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 20:01:37 -0500

    added take_survey view/controller

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -11,6 +11,12 @@ get '/survey/:id' do
   erb :survey
 end
 
+get '/secret_key' do
+  # @survey = Survey.where(secret_key: params[:secret_key])
+  @survey = Survey.last
+  erb :take_survey
+end
+
 
 # POSTS =================================
 
@@ -20,3 +26,16 @@ post '/survey/create' do
   current_user.surveys << survey
   redirect "/survey/#{survey.id}/create"
 end
+
+post '/survey/submit' do
+  @survey = Survey.find(params[:survey_id].to_i)
+  @participation = current_user.participations.where(survey_id: @survey.id).first
+  @responses = params[:response]
+
+  @responses.each do |question_id, choice_id|
+    Response.create(choice_id: choice_id, participation_id: @participation.id)
+  end
+
+end
+
+# user.participations.where(survey_id ==?)
